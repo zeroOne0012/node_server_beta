a4ee9299a7410fa227c883643d3f7a7f1c609c5b
zeroOne0012 <012012012il@gmail.com>
2025-01-06 12:47:24 +0900

    룸 관리 소켓 완

--- server/ws_eggtec.js
+++ server/ws_eggtec.js
@@ -9,57 +9,80 @@ const roomState = {
 let connectedUsers = 0;
 
 module.exports = (server) => {
-  const io = SocketIO(server, { 
-    cors: {
-        origin: '*', 
-    },
-   });
-  io.on('connection', async (socket) => {
-
-    // connect
-    connectedUsers++;
-    console.log('Client connected:', connectedUsers, socket.id);
-    io.emit('message', connectedUsers);
-
-    // disconnect
-    socket.on('disconnect', function(){
-        connectedUsers--;
-        console.log('Client disconnected', connectedUsers);
+    const serverIO = SocketIO(server, { 
+        cors: {
+            origin: '*', 
+        },
     });
 
-    // error
-    socket.on('error', (err) => {
-        console.error(err);
-     });
-    
-    // room state reply
-    socket.on('message', async (msg) => {
-        try{
-            const roomName = JSON.parse(msg).room;
-            socket.join(roomName);
-            socket.emit('message', {state:roomState[roomName], 'message':getRoomState(roomName, roomState[roomName])});
-        }catch(err){
-            socket.emit('message', 'Invalid message format');
-        }
-    });
+    const roomStateUpdate = serverIO.of('/roomStateUpdate');
 
-    // change test
-    socket.on('message', (data) => {
-        try {
-            const { event, roomName, newState } = JSON.parse(data);
-            if (event === 'updateRoomState') {
-                if (rooms[roomName] && [0, 1, 2].includes(newState)) {
-                    rooms[roomName].state = newState;
-                    io.to(roomName).emit('roomState', newState);
-                } else {
-                    socket.emit('error', 'Invalid room or state');
+    roomStateUpdate.on('connection', async (socket) => {
+
+        // connect
+        console.log('admin connected:', socket.id);
+
+        // disconnect
+        socket.on('disconnect', function(){
+            console.log('disconnected');
+        });
+
+        // error
+        socket.on('error', (err) => {
+            console.error(err);
+        });
+
+        // change test
+        socket.on('message', (data) => {
+            try {
+                const { event, roomName, newState } = JSON.parse(data);
+                if (event == 'updateRoomState') {
+                    if (roomName in roomState && [0, 1, 2].includes(newState)) {
+                        roomState[roomName].state = newState;
+                        roomStateListen.to(roomName).emit('message', newState);
+                        socket.emit('message', {changed:newState});
+                    } else {
+                        socket.emit('message', 'Invalid room or state');
+                    }
                 }
+            } catch (error) {
+                socket.emit('message', 'Invalid JSON format');
             }
-        } catch (error) {
-            socket.emit('error', 'Invalid JSON format');
-        }
+        });
+
     });
-    
-  });
-  
+
+    const roomStateListen = serverIO.of('/roomState');
+    roomStateListen.on('connection', async (socket) => {
+
+        // connect
+        connectedUsers++;
+        console.log('Client connected:', connectedUsers, socket.id);
+        roomStateListen.emit('message', connectedUsers);
+
+        // disconnect
+        socket.on('disconnect', function(){
+            connectedUsers--;
+            console.log('Client disconnected', connectedUsers);
+        });
+
+        // error
+        socket.on('error', (err) => {
+            console.error(err);
+            });
+
+        // room state reply
+        socket.on('message', async (msg) => {
+            try{
+                const roomName = JSON.parse(msg).room;
+                socket.join(roomName);
+                socket.emit('message', {state:roomState[roomName], 'message':getRoomState(roomName, roomState[roomName])});
+            }catch(err){
+                socket.emit('message', 'Invalid message format');
+            }
+        });
+
+
+    });
+
 };
